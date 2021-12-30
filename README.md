b481badcc6db748b3e222c113eb419b14ef4cbca
Carlos E <elizaldc@protonmail.com>
2021-12-30 16:21:06 -0600

    Made basic functions for math and added an operator function.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+Started on 12/23/21 by Carlos Elizalde
+The purpose of this program is to create a responsive calculator application.
--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operator(operand, num1, num2) {
+    let sum = 0;
+    switch(operand) {
+        case 'x':
+            sum = multiply(num1, num2);
+            break;
+        case '/':
+            sum = divide(num1, num2);
+            break;
+        case '+':
+            sum = add(num1, num2);
+            break;
+        case '-':
+            sum = subtract(num1, num2);
+            break;
+        default: 
+            return "Please enter a valid value.";
+    }
+    return sum;
+}
