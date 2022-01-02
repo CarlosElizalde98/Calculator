97f8740abbbcf227a2059e9084ba1908271cc2e6
Carlos E <elizaldc@protonmail.com>
2022-01-02 14:37:46 -0600

    Made changes to HTML and CSS and added calculator display logic in JS file.

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-const display = document.querySelector('#display');
+const currentDisplay = document.querySelector('#display');
 const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
+const numButtons = document.querySelectorAll('.num-button');
+const opButtons = document.querySelectorAll('.operator');
+const operationDisplay = document.getElementById('#lastDisplay');
 
+let firstNum = '';
+let secondNum = '';
+let shouldResetScreen = false;
 
+//Add button listeners for calculator buttons
+numButtons.forEach((button) =>
+    button.addEventListener('click', () => displayNumber(button.textContent))
+);
+
+opButtons.forEach((button) => 
+    button.addEventListener('click', () => displayOperation(button.textContent))
+);
+
+//Display number on the Calculator
+function displayNumber(number) {
+    if (currentDisplay.textContent === '0' || shouldResetScreen) {
+        resetDisplay();
+    }
+    currentDisplay.textContent += number;
+}
+
+function resetDisplay() {
+    currentDisplay.textContent = '';
+    shouldResetScreen = false;
+}
+
+//Basic operations for calculator
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -19,6 +48,7 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+//Determines numbers and operator chosen and performs calculation.
 function operator(operand, num1, num2) {
     let sum = 0;
     switch(operand) {
