f405560664eeac4e9408a8f6a4a1a5e97d0c891a
Carlos E <elizaldc@protonmail.com>
2022-01-04 14:52:37 -0600

    Made functions that can evaluate expressions made by User. Also made changes to HTML and style.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const currentDisplay = document.querySelector('#display');
-const clear = document.querySelector('#clear');
-const equals = document.querySelector('#equals');
+const clearBtn = document.querySelector('#clear');
+const equalsBtn = document.querySelector('#equals');
 const numButtons = document.querySelectorAll('.num-button');
 const opButtons = document.querySelectorAll('.operator');
-const operationDisplay = document.getElementById('#lastDisplay');
+const operationDisplay = document.querySelector('#lastDisplay');
 
 let firstNum = '';
 let secondNum = '';
+let currentOperator = null;
 let shouldResetScreen = false;
+
+equalsBtn.addEventListener('click', evaluate);
+clearBtn.addEventListener('click', clear);
 
 //Add button listeners for calculator buttons
 numButtons.forEach((button) =>
@@ -26,9 +30,42 @@ function displayNumber(number) {
     currentDisplay.textContent += number;
 }
 
+//Clears screen when clicked
+function clear() {
+    currentDisplay.textContent = '0';
+    lastDisplay.textContent = '';
+    firstNum = '';
+    secondNum = '';
+    currentOperator = null;
+}
+
+//Clears screen whenever a number is evaluated.
 function resetDisplay() {
     currentDisplay.textContent = '';
     shouldResetScreen = false;
+}
+
+//Displays operator alongside first operand.
+function displayOperation(operator) {
+    if (currentOperator !== null) {
+        evaluate();
+    }
+    firstNum = currentDisplay.textContent;
+    currentOperator = operator;
+    currentDisplay.textContent = `${firstNum} ${currentOperator}`;
+    shouldResetScreen = true;
+}
+
+//Evaluates expression entered by user.
+function evaluate() {
+    if (currentOperator === null || shouldResetScreen) {
+        return;
+    }
+
+    secondNum = currentDisplay.textContent;
+    currentDisplay.textContent = operator(currentOperator, firstNum, secondNum);
+    currentOperator = null;
+
 }
 
 //Basic operations for calculator
@@ -51,6 +88,8 @@ function divide(num1, num2) {
 //Determines numbers and operator chosen and performs calculation.
 function operator(operand, num1, num2) {
     let sum = 0;
+    num1 = Number(num1);
+    num2 = Number(num2);
     switch(operand) {
         case 'x':
             sum = multiply(num1, num2);
@@ -65,7 +104,7 @@ function operator(operand, num1, num2) {
             sum = subtract(num1, num2);
             break;
         default: 
-            return "Please enter a valid value.";
+            return null;
     }
     return sum;
 }
